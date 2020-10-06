2569db7c31e9a8506632251f376718e5b5b64126
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-06 12:16:10 +0530

    Updated init script

--- app/scripts/init.sh
+++ app/scripts/init.sh
@@ -1,5 +1,9 @@
 rm -f db.sqlite3
+find . -name __pycache__ -exec rm -rf {} +
+find ./api/migrations -type f ! -name __init__.py -exec rm -rf {} +
 pip install -Ur requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+python manage.py createsuperuser --username "admin" --email ""
+echo "Username: admin"
+echo "Login URL: http://127.0.0.1:8000/api/v1/auth/login/"
