6dba324d25bcfa47ef38ec82fdfa114a1148df3d
Nikhil John <nikhiljohn1010@gmail.com>
2020-10-07 12:09:14 +0530

    Fixed API Root

--- app/config/urls.py
+++ app/config/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-	path('', RedirectView.as_view(url='/'+settings.API_ROOT_URL)),
-	path('api/', RedirectView.as_view(url='/'+settings.API_ROOT_URL)),
-	# TODO: Improve root redirection method
+	re_path(r'^(api/)?$', RedirectView.as_view(url='/'+settings.API_ROOT_URL)),
 	path(settings.API_ROOT_URL, include('api.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
